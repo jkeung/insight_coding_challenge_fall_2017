be20fc65cd679936e92338114b2b2e9ef4fa876e
Jason <jason.s.keung@gmail.com>
2017-10-30 01:14:39 -0400

    make pep8 compliant

--- src/find_political_donors.py
+++ src/find_political_donors.py
@@ -1,7 +1,7 @@
-from sys import argv    
-import time             
-import contributions           
-# Program that calculates the median number of unique words per tweet
+from sys import argv
+import time
+import contributions
+# Program that calculates the median number
 # Author: Jason Keung
 # Created: October 29, 2017
 
@@ -9,27 +9,29 @@ import contributions
 if __name__ == "__main__":
     script, infile, outfile_zip, outfile_date = argv
     start_time = time.time()
-    print ("Starting median_unique.py...")
-    
-    my_contribution = contributions.ContributionManager(infile, outfile_zip, outfile_date)
-    
+    print("Starting find_political_donors.py...")
+
+    my_contribution = contributions.ContributionManager(
+        infile, outfile_zip, outfile_date)
+
     # stream
     while my_contribution.read_contribution():
 
         my_contribution.create_contribution()
-        
+
         if my_contribution.contribution.other_id == '':
             my_contribution.update_running_median()
             my_contribution.write_stats('zip_code')
-            
+
         else:
             pass
-    
-    # batch    
+
+    # batch
     my_contribution.write_stats('date')
 
     my_contribution.close()
 
-    # print ("Output is saved to %s ") % (outfile)
-    print ("median_unique.py run successfully!")
-    print ("--- %s seconds ---\n") % (time.time() - start_time)
+    print("Median values by zip code is saved to {0} ") % (outfile_zip)
+    print("Median values by date is saved to {0} ") % (outfile_date)
+    print("find_political_donors.py run successfully!")
+    print("--- %s seconds ---\n") % (time.time() - start_time)
